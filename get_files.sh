73a5ec0a8a77bb2933b3f72c252256fad83b087c
Houssem-der <houssemeddine.derbel@unlv.edu>
2025-07-24 13:37:59 -0700

    Create get_files.sh

---
+++ get_files.sh
@@ -0,0 +1,14 @@
+# for rmsk.txt file
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/rmsk.txt.gz
+gunzip rmsk.txt.gz
+
+# for gencode.v41.chr_patch_hapl_scaff.annotation.gtf file
+wget wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_41/gencode.v41.chr_patch_hapl_scaff.annotation.gtf.gz
+gunzip gencode.v41.chr_patch_hapl_scaff.annotation.gtf.gz
+
+# for encodeCcreCombined
+wget https://hgdownload.soe.ucsc.edu/gbdb/hg38/encode3/ccre/encodeCcreCombined.bb
+wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/bigBedToBed
+chmod +x bigBedToBed
+./bigBedToBed encodeCcreCombined.bb encodeCcreCombined.bed
+rm ./bigBedToBed
